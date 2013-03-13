331e32af594f4c34fdc729a16af606fbd3aee726
Hayden Wood <haydwood@tamu.edu>
2013-03-12 20:58:04 -0500

    each position on the board has a state

--- Fanorona/src/team01/Board.java
+++ Fanorona/src/team01/Board.java
@@ -6,9 +6,30 @@ public class Board {
 
 	static final int BOARD_SIZE = 9 * 5;
 	
+	// board states
+	static final int EMPTY = 0;
+	static final int WHITE = 1;
+	static final int BLACK = 2;
+	
 	private List <Integer> board;
 	
 	public Board() {
 		board = new ArrayList <Integer>(BOARD_SIZE);
+	}
+	
+	boolean isEmpty(int pos) {
+		return board.get(pos) == EMPTY;
+	}
+	
+	boolean isWhite(int pos) {
+		return board.get(pos) == WHITE;
+	}
+	
+	boolean isBlack(int pos) {
+		return board.get(pos) == BLACK;
+	}
+	
+	void setPosition(int pos, int state) {
+		board.set(pos, state);
 	}
 }
