5faef08554a522113c887281f709efb148a3b9ee
Hayden Wood <haydwood@tamu.edu>
2013-03-12 20:24:42 -0500

    added board class

---
+++ Fanorona/src/team01/Board.java
@@ -0,0 +1,14 @@
+package team01;
+
+import java.util.*;
+
+public class Board {
+
+	static final int BOARD_SIZE = 9 * 5;
+	
+	private List <Integer> board;
+	
+	public Board() {
+		board = new ArrayList <Integer>(BOARD_SIZE);
+	}
+}
