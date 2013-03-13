dacb863b975ccec14d75b7b1a59eda29fecb21e3
Hayden Wood <haydwood@tamu.edu>
2013-03-12 22:50:02 -0500

    i want to have some testing for the Board class

---
+++ Fanorona/src/team01/BoardTest.java
@@ -0,0 +1,16 @@
+package team01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void test() {
+		Board board = new Board();
+		
+		System.out.println (board);
+	}
+
+}
