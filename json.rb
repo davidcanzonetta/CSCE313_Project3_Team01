8bdd1fe4e2ff0c353e392fe391599034b8016f35
Mariano Salem <msalem@tamu.edu>
2013-04-29 14:23:18 -0500

    File now gets post title and image url from json

--- json.rb
+++ json.rb
@@ -3,11 +3,25 @@ require 'pp'
 require 'net/http'
 require 'open-uri'
 
-url = 'http://reddit.com/r/pics.json'   #url
+url = 'http://reddit.com/.json'   #url
 
 buffer = open(url).read
 
 result = JSON.parse(buffer)
 
-pp result                               #display
+#result1 = result['data']['children'][0]['data']['thumbnail']
+
+result['data']['children'].each {
+  |x|
+
+  puts x['data']['title']
+  puts x['data']['url']
+  puts '   '
+}
+                              #display
+
+
+
+
+
 
