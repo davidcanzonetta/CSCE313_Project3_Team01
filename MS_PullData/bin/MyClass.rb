ae19c55990e02ddb0131f7891ba7c3c511d577b8
davidcanzonetta <davidcanzonetta@tamu.edu>
2013-05-02 14:05:25 -0500

    Login Window
    Added Log in window. Does not have functionality yet in terms of reading password and account

--- MS_PullData/bin/MyClass.rb
+++ MS_PullData/bin/MyClass.rb
@@ -1,48 +1,22 @@
-require 'json'
-require 'open-uri'
-
 class MyClass #(change name)
-
 	include GladeGUI
-
-	@@title = Array.new
-  @@link = Array.new
-
-  def initialize()
-
-    @url = 'http://reddit.com/.json'
-
-    buffer = open(@url).read
-
-    result = JSON.parse(buffer)
-
-    result['data']['children'].each {
-        |x|
-
-      @@title.push(x['data']['title'])
-      @@link.push(x['data']['url'])
-    }
-  end
 
 	def show()
 		load_glade(__FILE__)  #loads file, glade/MyClass.glade into @builder
-		@linkbutton1 = @@title[0]
-		@linkbutton2 = @@title[1]
-		@linkbutton3 = @@title[2]
-		@linkbutton4 = @@title[3]
-		@linkbutton5 = @@title[4]
-		@linkbutton6 = @@title[5]
-		@linkbutton7 = @@title[6]
-		@linkbutton8 = @@title[7]
-		
-		set_glade_all() #populates glade controls with insance variables (i.e. Myclass.label1) 
-		show_window() 
+		@builder["window1"].title = "MyClass" # appears in window title bar
+		show_window()  #calls Gtk.main
 	end	
 
-	def linkbutton1__clicked(*argv)
+	def loginButton__clicked(*argv)
+		reddit_win = Reddit.new() #self = parent
+		reddit_win.show(self)
+	end
 
-
+	# This method is unnecessary, see MyModalClass.
+	def cancelButton__clicked(*argv)
+		destroy_window()
 	end
 
 end
+
 
