88750f2f215380473a2a8c7fb96416f04642c5ec
davidcanzonetta <davidcanzonetta@tamu.edu>
2013-05-05 22:12:57 -0500

    Updated Reddit.rb
    Can now read and write to a json file using visualruby.

--- MS_PullData/bin/Reddit.rb
+++ MS_PullData/bin/Reddit.rb
@@ -7,11 +7,22 @@ class Reddit #(change name)
 
 	include GladeGUI
 
+	@@page = 0							#page will be used to browse along different pages, since title and link array contain more than
+													#7 posts from the homepage (title might have 27 entries)
+	@@check_variable = 0 #dummy variable	
+
 	@@title = Array.new
   @@link = Array.new
 
-  def initialize()
+	#Parse Favorites Json File
+	json_buffer = File.read("favorites.json")
+	@@topObject = JSON.parse(json_buffer)
+	@@json_titles = @@topObject["titles"]
+	@@json_urls = @@topObject["urls"]
 
+	def initialize()
+		
+		#Parse Reddit Json
     @url = 'http://reddit.com/.json'
 
     buffer = open(@url).read
@@ -23,47 +34,164 @@ class Reddit #(change name)
 
       @@title.push(x['data']['title'])
       @@link.push(x['data']['url'])
-    }
+    }	
   end
 
-	def show(parent)
-		load_glade(__FILE__, parent)  #loads file, glade/MyClass.glade into @builder
-		@linkbutton1 = @@title[0]
-		@linkbutton2 = @@title[1]
-		@linkbutton3 = @@title[2]
-		@linkbutton4 = @@title[3]
-		@linkbutton5 = @@title[4]
-		@linkbutton6 = @@title[5]
-		@linkbutton7 = @@title[6]
-		@linkbutton8 = @@title[7]
+	def show()
+		load_glade(__FILE__)  #loads file, glade/MyClass.glade into @builder
+
+		@linkbutton1 = @@title[0 + @@page]		#at the beginning, we are using title[0+0], if we press next, page becomes 8 and this becomes title[8]
+		@linkbutton2 = @@title[1 + @@page]
+		@linkbutton3 = @@title[2 + @@page]
+		@linkbutton4 = @@title[3 + @@page]
+		@linkbutton5 = @@title[4 + @@page]
+		@linkbutton6 = @@title[5 + @@page]
+		@linkbutton7 = @@title[6 + @@page]
+		@linkbutton8 = @@title[7 + @@page]
+
 		set_glade_variables(self) # fills label with message
-		@builder["window1"].title = "Reddit App" 
+		@builder["window1"].title = "Reddit App"
 		show_window()
 	end	
 
+	#save arrays to json file
+	def save_arrays(num)
+		#ensure title and url have not already been written
+		if @@json_titles.size == 0
+			@@json_titles.push @@title[num]
+		else
+			@@json_titles.each do |x|
+				if x == @@title[num]
+						@@check_variable = 1
+				end
+			end
+			if @@check_variable != 1
+				@@json_titles.push @@title[num]
+			end
+			@@check_variable = 0
+		end
+		if @@json_urls.size == 0
+			@@json_urls.push @@link[num]
+		else
+			@@json_urls.each do |x|
+				if x == @@link[num]
+					@@check_variable = 1
+				end	
+			end
+			if @@check_variable != 1
+				@@json_urls.push @@link[num]
+			end
+			@@check_variable = 0
+		end
+		
+		#write to json file
+		File.open("favorites.json", "w") do |f|
+			f.write(@@topObject.to_json)
+		end
+		#f.close
+	end
+
 	def linkbutton1__clicked(*argv)
-		Launchy.open(@@link[0])
+		if @@link[0+@@page] == nil				#if there is no link in the array do nothing when the button is pressed
+		else															#else show the link
+			Launchy.open(@@link[0+@@page])
+		end
 	end
+
 	def linkbutton2__clicked(*argv)
-		Launchy.open(@@link[1])
+		if @@link[1+@@page] == nil
+		else	
+			Launchy.open(@@link[1+@@page])
+		end
 	end
+
 	def linkbutton3__clicked(*argv)
-		Launchy.open(@@link[2]])
+		if @@link[2+@@page] == nil
+		else	
+			Launchy.open(@@link[2+@@page])
+		end
 	end
+
 	def linkbutton4__clicked(*argv)
-		Launchy.open(@@link[3])
+		if @@link[3+@@page] == nil
+		else	
+			Launchy.open(@@link[3+@@page])
+		end
 	end
+
 	def linkbutton5__clicked(*argv)
-		Launchy.open(@@link[4])
+		if @@link[4+@@page] == nil
+		else	
+			Launchy.open(@@link[4+@@page])
+		end
 	end
+
 	def linkbutton6__clicked(*argv)
-		Launchy.open(@@link[5])
+		if @@link[5+@@page] == nil
+		else	
+			Launchy.open(@@link[5+@@page])
+		end
 	end
+
 	def linkbutton7__clicked(*argv)
-		Launchy.open(@@link[6])
+		if @@link[6+@@page] == nil
+		else	
+			Launchy.open(@@link[6+@@page])
+		end
 	end
+
 	def linkbutton8__clicked(*argv)
-		Launchy.open(@@link[7])
+		if @@link[7+@@page] == nil
+		else	
+			Launchy.open(@@link[7+@@page])
+		end
+	end
+
+	def button2__clicked(*argv)				#Next button, when pressed, it will display the next page in the reddit page
+		if @@page == 0
+		else
+			@@page = @@page - 8
+			self.destroy_window	
+			self.show
+		end 
+	end
+
+	def button3__clicked(*argv)				#Next button, when pressed, it will display the next page in the reddit page
+		@@page = @@page + 8
+		self.destroy_window	
+		self.show
+	end
+
+	#When save button pressed, that title and link are saved
+	def savebutton1__clicked(*argv)
+		save_arrays(0)			
+	end
+	
+	def savebutton2__clicked(*argv)	
+		save_arrays(1)			
+	end
+
+	def savebutton3__clicked(*argv)
+		save_arrays(2)				
+	end
+
+	def savebutton4__clicked(*argv)
+		save_arrays(3)				
+	end
+
+	def savebutton5__clicked(*argv)	
+		save_arrays(4)			
+	end
+
+	def savebutton6__clicked(*argv)
+		save_arrays(5)			
+	end
+
+	def savebutton7__clicked(*argv)		
+		save_arrays(6)			
+	end
+
+	def loadfavorites_clicked(*argv)
 	end
 
 end
