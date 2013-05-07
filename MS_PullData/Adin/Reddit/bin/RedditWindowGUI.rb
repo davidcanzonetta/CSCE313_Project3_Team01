0b5c6f0df0d59ee60ba813f0bee8c7b8d673787f
davidcanzonetta <davidcanzonetta@tamu.edu>
2013-05-06 20:25:53 -0500

    Updated RedditWindowGUI.rb
    Added from_child definition

--- MS_PullData/Adin/Reddit/bin/RedditWindowGUI.rb
+++ MS_PullData/Adin/Reddit/bin/RedditWindowGUI.rb
@@ -2,14 +2,19 @@ class RedditWindowGUI #< RedditWindow #(change name)
 
 	include GladeGUI
 
-	def show()
+	def show(parent)
 		load_glade(__FILE__)
+		set_glade_variables(self)
 		show_window()
 	end
 
 	def button1__clicked(*argv) 
-		@twitterWin = TwitterLoginWindow.new() #self = parent
+		@twitterWin = TwitterLoginWindow.new(self) #self = parent
 		@twitterWin.show(self)
+	end
+	
+	def from_child(client)
+		client.update("Tweeting from ruby")
 	end
 
 end
